8e4ce18f673dd560363a7f4d81529f0fd3f87fc1
Atharv Deshmukh <52416614+Atharv-Deshmukh@users.noreply.github.com>
2020-08-27 18:29:47 +0530

    Add files via upload

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next){
+    const token = req.headers.authorization.split(" ")[1];
+    if(!token){
+        res.redirect('login')
+    }
+    else{
+        try{
+            const verified = jwt.verify(token, "ThisIsTopSecret");
+            console.log(verified)
+            req.user = verified;
+            next()
+        }
+        catch(err){
+            send('Invalid Token')
+        }
+    }
+}
